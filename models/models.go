f413e3ae3850e7c13a05c64f7706bd339f28f381
Dela Ricch <delaricch@gmail.com>
2024-01-05 10:45:29 +0000

    feat: added user and comment models

--- models/models.go
+++ models/models.go
@@ -6,6 +6,20 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+type User struct {
+	Id primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name string `json:"name" bson:"name"`
+	Email string `json:"email" bson:"email"`
+	Password string `json:"password" bson:"password"`
+	CreatedAt    time.Time `json:"createdAt" bson:"createdAt"`
+	UpdatedAt    time.Time `json:"updatedAt" bson:"updatedAt"`
+}
+
+type Comment struct {
+	Id primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	FeedbackId primitive.ObjectID `json:"feedbackId" bson:"feedbackId"`
+	UserId primitive.ObjectID `json:"userId" bson:"userId"`
+}
 
 type Feedback struct {
 	Id primitive.ObjectID `json:"id" bson:"_id,omitempty"`
@@ -13,6 +27,7 @@ type Feedback struct {
 	Category string `json:"category" bson:"category"`
 	Details string `json:"details" bson:"details"`
 	Upvotes int `json:"upvotes" bson:"upvotes"`
+	Comments []Comment `json:"comments" bson:"comments"`
 	CreatedAt    time.Time `json:"createdAt" bson:"createdAt"`
     UpdatedAt    time.Time `json:"updatedAt" bson:"updatedAt"`
 }
